3ef530d283d1a43e4dbce8b52ba9a309a0f740c1
raulmf9325 <raulmf9325@ufl.edu>
2019-03-02 14:11:08 -0500

    Initial Commit

---
+++ Bluetooth-Handshake-PersistentWrites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bluetooth-Handshake-PersistentWrites
+//
+//  Created by Raul Mena on 3/2/19.
+//  Copyright © 2019 Raul Mena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
